3d1c3af92e42d58184fa1ec918f197fe3974858c
Tomas Jancik <t.jancik@gmail.com>
2019-10-23 21:57:13 +0200

    add code for wifi communication - commented

--- ClassRating.ino
+++ ClassRating.ino
@@ -1,4 +1,12 @@
+//#include <WiFi.h>
+//#include <HTTPClient.h>
+
 #include "LedControl.h"
+
+const char* ssid = "";
+const char* password =  "";
+
+bool sent = false;
 
 // nastavení propojovacích pinů
 #define DIN 10
@@ -79,6 +87,19 @@ void setup() {
   pinMode(btnBad, INPUT);
 
   Serial.begin(9600);
+
+//  delay(4000);   //Delay needed before calling the WiFi.begin
+//
+//  Serial.println("Connecting to WiFi..");
+//  WiFi.begin(ssid, password); 
+// 
+//  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+//    delay(100);
+//    Serial.print(".");
+//  }
+//
+//  Serial.println("");
+//  Serial.println("Connected to the WiFi network");
 }
 
 void loop() {
@@ -132,5 +153,34 @@ void loop() {
     }
 
     refresh = false;
+
+
+//    if(!sent) {
+//    HTTPClient http;
+//    http.begin("http://hwi.tomasjancik.net/"); 
+//    http.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"); //Specify content-type header
+//    http.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3");
+//    http.addHeader("Accept-Language", "en-US,en;q=0.9,cs;q=0.8");
+//    http.addHeader("Cache-Control", "max-age=0");
+//    http.addHeader("Connection", "keep-alive");
+//    http.addHeader("DNT", "1");
+//    http.addHeader("Host", "hwi.tomasjancik.net");
+//    int httpResponseCode = http.GET(); //Send the actual POST request
+//    sent = true;
+//    if(httpResponseCode>0){
+//     
+//        String response = http.getString();  //Get the response to the request
+//     
+//        Serial.println(httpResponseCode);   //Print return code
+//        Serial.println(response);           //Print request answer
+//     
+//    }else{
+//     
+//        Serial.print("Error on sending GET: ");
+//        Serial.println(httpResponseCode);
+//     
+//    }
+//    
+//    http.end();
   }
 }
