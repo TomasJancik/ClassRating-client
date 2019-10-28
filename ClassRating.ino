bd2d3cf32e85c881456a49ba642c2b8377987112
Tomas Jancik <t.jancik@gmail.com>
2019-10-28 21:20:41 +0100

    just some comments, uncommenting wifi communication

--- ClassRating.ino
+++ ClassRating.ino
@@ -1,25 +1,21 @@
-// @TODO need to change LED matrix pins to work with ESP
-// uncomment WiFi communication code when ready
-
-
-//#include <WiFi.h>
-//#include <HTTPClient.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 #include "LedControl.h"
 
-const char* ssid = "";
-const char* password =  "";
+const char* ssid = ""; // set WiFi network SSID
+const char* password =  ""; // set Wifi password
 
 bool sent = false;
 
 // nastavení propojovacích pinů
-#define DIN 10
-#define CS  11
-#define CLK 12
+#define DIN 13
+#define CS  12
+#define CLK 14
 
-#define btnHappy  2
-#define btnMeh    4
-#define btnBad    6
+#define btnHappy  32
+#define btnMeh    35
+#define btnBad    34
 
 // tato proměnná údává počet matic,
 // počítání začíná od nuly
@@ -79,36 +75,67 @@ void blink(byte smile[8], int speed) {
 }
 
 void setup() {
-  // probuzení matice pro zahájení komunikace
+  // setup LED matrix
   ledMat.shutdown(mojeMat,false);
-  // nastavení LED matice na střední svítivost (0-15)
   ledMat.setIntensity(mojeMat,7);
-  // vypnutí všech LED diod na matici
   ledMat.clearDisplay(mojeMat);
 
+  // set button pins
   pinMode(btnHappy, INPUT);
   pinMode(btnMeh, INPUT);
   pinMode(btnBad, INPUT);
 
   Serial.begin(9600);
 
-//  delay(4000);   //Delay needed before calling the WiFi.begin
-//
-//  Serial.println("Connecting to WiFi..");
-//  WiFi.begin(ssid, password); 
-// 
-//  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
-//    delay(100);
-//    Serial.print(".");
-//  }
-//
-//  Serial.println("");
-//  Serial.println("Connected to the WiFi network");
+  delay(4000);   //Delay needed before calling the WiFi.begin
+
+  Serial.println("Connecting to WiFi..");
+  WiFi.begin(ssid, password); 
+ 
+  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+    delay(100);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Connected to the WiFi network");
+}
+
+void sendRequest(String rating) {
+    HTTPClient http;
+    String url = "http://example.com/?rating=" + rating;
+    Serial.println(rating);
+    Serial.println(url);
+    http.begin(url); 
+    http.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"); //Specify content-type header
+    http.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3");
+    http.addHeader("Accept-Language", "en-US,en;q=0.9,cs;q=0.8");
+    http.addHeader("Cache-Control", "max-age=0");
+    http.addHeader("Connection", "keep-alive");
+    http.addHeader("DNT", "1");
+    http.addHeader("Host", "emaple.com");
+    int httpResponseCode = http.GET(); //Send the actual POST request
+    if(httpResponseCode>0){
+     
+        String response = http.getString();  //Get the response to the request
+     
+        Serial.println(httpResponseCode);   //Print return code
+        Serial.println(response);           //Print request answer
+     
+    }else{
+     
+        Serial.print("Error on sending GET: ");
+        Serial.println(httpResponseCode);
+     
+    }
+    
+    http.end();
 }
 
 void loop() {
   if(digitalRead(btnHappy) == HIGH && debounce()) {
     Serial.println("Pressed happy button");
+    sendRequest("1");
     lastBtn = btnHappy;
     total += 1;
     btnPressed +=1;
@@ -116,6 +143,7 @@ void loop() {
     blink(smileHappy, 100);
   } else if(digitalRead(btnMeh) == HIGH && debounce()) {
     Serial.println("Pressed meh button");
+    sendRequest("2");
     lastBtn = btnMeh;
     total += 2;
     btnPressed +=1;
@@ -123,6 +151,7 @@ void loop() {
     blink(smileMeh, 100);
   } else if(digitalRead(btnBad) == HIGH && debounce()) {
     Serial.println("Pressed bad button");
+    sendRequest("3");
     lastBtn = btnBad;
     total += 3;
     btnPressed +=1;
@@ -157,34 +186,5 @@ void loop() {
     }
 
     refresh = false;
-
-
-//    if(!sent) {
-//    HTTPClient http;
-//    http.begin("http://hwi.tomasjancik.net/"); 
-//    http.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"); //Specify content-type header
-//    http.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3");
-//    http.addHeader("Accept-Language", "en-US,en;q=0.9,cs;q=0.8");
-//    http.addHeader("Cache-Control", "max-age=0");
-//    http.addHeader("Connection", "keep-alive");
-//    http.addHeader("DNT", "1");
-//    http.addHeader("Host", "hwi.tomasjancik.net");
-//    int httpResponseCode = http.GET(); //Send the actual POST request
-//    sent = true;
-//    if(httpResponseCode>0){
-//     
-//        String response = http.getString();  //Get the response to the request
-//     
-//        Serial.println(httpResponseCode);   //Print return code
-//        Serial.println(response);           //Print request answer
-//     
-//    }else{
-//     
-//        Serial.print("Error on sending GET: ");
-//        Serial.println(httpResponseCode);
-//     
-//    }
-//    
-//    http.end();
   }
 }
